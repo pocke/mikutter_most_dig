24a2b0a383e39d77ea9d9290339ca5ece5f42fda
pocke <p.ck.t22@gmail.com>
2013-05-26 17:08:17 +0900

    sleepが動いてない

--- most_dig.rb
+++ most_dig.rb
@@ -17,10 +17,9 @@ Plugin.create(:most_dig) do
       doc.css('a.fs-date').each do |elm|
         tweet_id = elm['href'].split('/')[-1]
         (Service.primary.twitter/'favorites/create').json(:id => tweet_id)
-
         (Service.primary.twitter/"statuses/retweet/#{tweet_id}").json( :trim_user => true )
         i += 1
-        sleep 2
+        sleep 2    #掘り起こす間隔。連投したら申し訳ないじゃないですかー #うまく動かないけどね(白目
         break if i == 10  #20RTもしたら厄介でしかないから10RTに制限。限界突破したかったらこの行消してね
       end
     end
